57c8e0f494943965a09bd3be095d8a4bbaf060fb
Evan Light <evan.light@tripledogdare.net>
2013-02-17 17:06:08 -0500

    Modified to dabble with serially executing sync_tasks

--- plugins/on_login_experiment.rb
+++ plugins/on_login_experiment.rb
@@ -1,11 +1,26 @@
 class OnLoginExperiment
   include Purugin::Plugin, Purugin::Tasks
-  
   description 'OnLoginExperiment', 0.1
+
+  attr_accessor :world
   
   def on_enable 
     event(:player_login) do |e|
-      sync_task(10) { e.player.world.players.each { |p| p.send_message 'P0wnd' } }
+      self.world = e.player.world
+      sync_task(10) do
+        message_all("pwned")
+        sync_task(1) do
+          message_all("bitch")
+        end
+      end
+    end
+  end
+
+  private
+
+  def message_all(msg)
+    world.players.each do |p|
+      p.send_message msg
     end
   end
 end
