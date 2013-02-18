c3c542fa3299ef364f1bb2cc2aa6d292d421640e
Evan Light <evan.light@tripledogdare.net>
2013-02-18 13:09:24 -0500

    Adds my slightly tweaked Purugin jar
    Working to forward port my patch to Purugin master.

--- plugins/on_login_experiment.rb
+++ plugins/on_login_experiment.rb
@@ -7,12 +7,9 @@ class OnLoginExperiment
   def on_enable 
     event(:player_login) do |e|
       self.world = e.player.world
-      sync_task(10) do
-        message_all("pwned")
-        sync_task(1) do
-          message_all("bitch")
-        end
-      end
+      TaskChain.
+        task(10) { message_all("pwned") }.
+        task(1)  { message_all("bitch") }
     end
   end
 
@@ -21,6 +18,25 @@ class OnLoginExperiment
   def message_all(msg)
     world.players.each do |p|
       p.send_message msg
+    end
+  end
+
+  class TaskChain
+    include Purugin::Tasks
+    
+    attr_reader :delay
+    
+    def initialize(delay, &block)
+      @delay = delay
+      sync_task(delay, &block)
+    end
+
+    def self.task(delay = 0, &block)
+      TaskChain.new(delay, &block)
+    end
+    
+    def task(delay = 0, &block)
+      TaskChain.new(self.delay + delay, &block)
     end
   end
 end
